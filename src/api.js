25422bceaef9279c09e74e8e66c4a142f3a1acec
pratim440 <c.pratim440@gmail.com>
2022-02-01 22:24:01 +0530

    12th

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-export const api = "http://localhost:5000";
+export const api = "https://user-entry-mern.herokuapp.com";
 export const config = {
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
